0479ea92cf3ee445dad34d523e7ea0036892db63
Adrian Gaudebert <adrian@gaudebert.fr>
2013-06-28 22:06:37 +0200

    THE BASICS

---
+++ js/main.js
@@ -0,0 +1,7 @@
+(function(window) {
+    'use strict';
+    var canvas = document.getElementById('stage');
+    var context = canvas.getContext('2d');
+
+    context.fillRect(0, 0, 20, 20);
+}(window));
