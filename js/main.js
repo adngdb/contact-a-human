92a5a0aaa3ac014ad167e698db13b4efcd725c3d
Aurelien Defossez <defossez.aurelien@gmail.com>
2013-06-28 22:34:30 +0200

    Loading added.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
 (function(window) {
     'use strict';
+    var FPS = 30;
+    var CANVAS_WIDTH = 800;
+    var CANVAS_HEIGHT = 600;
+
     var canvas = document.getElementById('stage');
     var context = canvas.getContext('2d');
+    var dt = 1000 / FPS;
 
-    context.fillRect(0, 0, 20, 20);
+    var arcProgress = 0;
+
+    function update(dt) {
+        arcProgress += dt * 0.001;
+    }
+
+    function draw() {
+        context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+        context.beginPath();
+        context.arc(75, 75, 50, arcProgress * Math.PI, arcProgress * Math.PI - Math.PI / 2, true);
+        context.stroke();
+    }
+
+    setInterval(function() {
+        update(dt);
+        draw();
+    }, dt);
 }(window));
