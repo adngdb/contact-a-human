ca4d0733b1db17e6be884ec656c105c6335bdcca
Aurelien Defossez <defossez.aurelien@gmail.com>
2013-06-28 23:53:06 +0200

    Funcky C

--- js/main.js
+++ js/main.js
@@ -5,20 +5,28 @@
     var CANVAS_HEIGHT = 600;
 
     var canvas = document.getElementById('stage');
+    canvas.setAttribute("width", CANVAS_WIDTH);
+    canvas.setAttribute("height", CANVAS_HEIGHT);
     var context = canvas.getContext('2d');
     var dt = 1000 / FPS;
 
-    var arcProgress = 0;
+    var realTime = 0;
 
     function update(dt) {
-        arcProgress += dt * 0.001;
+        realTime += dt;
     }
 
     function draw() {
         context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
+        // var gameTime = realTime - Math.floor(realTime / 1000) * 200
+        var gameTime = realTime + Math.sin(realTime / 1000) * 2000;
+        // console.log("gameTime", realTime, gameTime)
+
+        var angle = (gameTime * .001) % 2 * Math.PI;
+
         context.beginPath();
-        context.arc(75, 75, 50, arcProgress * Math.PI, arcProgress * Math.PI - Math.PI / 2, true);
+        context.arc(75, 75, 50, angle, angle + 3 * Math.PI / 4, true);
         context.stroke();
     }
 
